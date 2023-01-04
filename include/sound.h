2174106bc4073f23c5621a564a75a6bdec57bd87
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2023-01-04 16:40:54 +0100

    add the .h file for the sound

---
+++ include/sound.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-100-REN-1-1-myhunter-ludwig.vandenberghe
+** File description:
+** struct
+*/
+#include <SFML/Graphics.h>
+#include <SFML/Audio.h>
+#include <SFML/System.h>
